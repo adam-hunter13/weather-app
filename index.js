21d2e4c4fab1b5ce0cbed818469556cead36792b
Adam Hunter <hunteradam13@gmail.com>
2021-12-01 14:32:54 -0700

    adding api key

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='';
+const API_KEY ='969b3a17508fcf9094c2b593bfa256b0';
 
 setInterval(() => {
     const time = new Date();
